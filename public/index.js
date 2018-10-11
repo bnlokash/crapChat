cf79dd95c65ed9299304ee73a500ced9114b2f20
Benjamin Lokash <benjamin@lokash.ca>
2018-10-11 17:56:26 -0400

    add bootstrap styling

--- public/index.js
+++ public/index.js
@@ -1,11 +1,17 @@
 $(()=>{
     var socket = io();
-    $('form').submit(()=>{
+    function sendMessage(){
         socket.emit('chatMessage', $('#messageInput').val());
         $('#messageInput').val('');
         return false;
-    });
+    }
+    $('#button-send').on('click', sendMessage);
+    $('#messageInput').keypress((e)=>{
+        if (e.which == 13){
+            sendMessage();
+        }
+    })
     socket.on('chatMessage', (messageText)=>{
-        $('#messages').append($('<li>').text(messageText));
+        $('#messages').append($('<li>').addClass('list-group-item').text(messageText));
     })
 });
