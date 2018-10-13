32bc72efc06deaf8430fd26f40b86b875a15a2d5
Benjamin Lokash <benjamin@lokash.ca>
2018-10-13 11:43:20 -0400

    implement current users div, add/remove on connect/disconect

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ var socket = require('socket.io');
 var io = socket(httpServer);
 
 var portNumber = 4200;
-let userCount = 0;
+var userCount = 0;
+var userConnectCount = 0;
+var users = [];
 
 app.use(express.static('public'));
 
@@ -18,19 +20,42 @@ app.get('/', (request, response)=> {
 
 io.on('connection', (socket)=>{
     userCount++;
+    userConnectCount++;
+
+    // assemble user info to emit 
+    var userID = socket.id;
+    var userName = "User" + userConnectCount;
+
+    // add new user to users array
+    users.push({id: userID, name: userName});
+
     console.log(userCount + ' users connected');
+    
+    // emit new user info, current users array
+    socket.emit('initUser', userName, userID, users);
 
+    // emit new user to all current users
+    socket.broadcast.emit('newUser', userName);
+
+    // wire-ups
+    // on chatMessage, re-emit
     socket.on('chatMessage', (messageText)=>{
-        console.log('New Message: ' + messageText);
         io.emit('chatMessage', messageText);
-    })
+    });
 
+    // on disconnect
+    //    - remove user from users array
     socket.on('disconnect', ()=> {
         userCount--;
-        console.log('user disconnected - ' + userCount + ' users remain');
+        users = users.filter((obj)=>{
+            return obj.id !== socket.id;
+        })
+        console.log( userName + ' disconnected - ' + userCount + ' users remain');
+        // broadcast removeUser containing userName for clients to remove from current users div
+        socket.broadcast.emit('removeUser', userName)
     });
-})
+});
 
 httpServer.listen(portNumber, ()=> {
     console.log('listening on ' + portNumber);
-})
+});
--- public/index.js
+++ public/index.js
@@ -1,17 +1,47 @@
 $(()=>{
     var socket = io();
-    function sendMessage(){
-        socket.emit('chatMessage', $('#messageInput').val());
+    var userName = "";
+    var userID = 0;
+    var currentUsers = [{}];
+
+    function sendMessage() {
+        socket.emit('chatMessage', userID + userName + ": " + $('#messageInput').val());
         $('#messageInput').val('');
         return false;
     }
+    // wire up sendMessage to send button and input box enter
     $('#button-send').on('click', sendMessage);
     $('#messageInput').keypress((e)=>{
         if (e.which == 13){
             sendMessage();
         }
-    })
+    });
+
+    // on chatMessage, append message text
     socket.on('chatMessage', (messageText)=>{
         $('#messages').append($('<li>').addClass('list-group-item').text(messageText));
+    });
+
+    // on client connection, server sends back an initUser containing a userName and ID, and current users array
+    socket.on('initUser', (name, id, users)=>{
+        userName = name;
+        userID = id;
+        for (var i = 0; i < users.length; i++) {
+            console.log(users[i]);
+            $('#currentUsers').append($('<li>').addClass('list-group-item').text(users[i].name));
+        }
+    });
+
+    // when another user connects, add their name to current users div
+    
+    socket.on('newUser', (name)=>{
+        $('#currentUsers').append($('<li>').addClass('list-group-item').text(name));
+    });
+
+    // when another user disconnects, remove their name from current users div
+    // TO-DO: should probably store other users id instead of removing by name string
+    socket.on('removeUser', (name)=>{
+        $('li:contains("' +name+ '")').remove();
     })
+
 });
